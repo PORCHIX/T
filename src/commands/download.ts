856a58007438ab97defd7848650862bc37a1d88a
Richard Hofmänner <hello@terillos.dev>
2020-05-29 19:06:49 +0200

    Sync

--- src/commands/download.ts
+++ src/commands/download.ts
@@ -1,34 +1,44 @@
 import {Context} from 'telegraf'
-import got from 'got'
+import Axios from 'axios'
 import validator from 'validator'
 import { ExtendedContext } from '../interfaces/ExtendedContext'
 import {promisify} from 'util'
 import {Stream} from 'stream'
 import * as fs from 'fs'
 const cheerio = require('cheerio')
-import { Response } from 'got/dist/source/core'
 
 export default async function (ctx: ExtendedContext) {
     if (validator.isURL(ctx.message.cmdArgs[1])) {
-        let res: Response = await got.get(ctx.message.cmdArgs[1], {followRedirect: true})
-        const redirectUrl: string = res.redirectUrls[0]
-        let resRedirect: Response = await got.get(redirectUrl, {
-            headers: {
-                'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) snap Chromium/81.0.4044.138 Chrome/81.0.4044.138 Safari/537.36'
-            }
+        Axios.get(ctx.message.cmdArgs[1],
+            {
+                headers: {
+                    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) snap Chromium/81.0.4044.138 Chrome/81.0.4044.138 Safari/537.36'
+                }
+            })
+        .then((res) => {
+            let $ = cheerio.load(res.data);
+            console.log(res.data);
+
+            ctx.reply('Success');
         })
-        let $ = cheerio.load(resRedirect.body)
+        // const redirectUrl: string = res.redirectUrls[0]
+        // let resRedirect: Response = await got.get(redirectUrl, {
+        //     headers: {
+        //         'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) snap Chromium/81.0.4044.138 Chrome/81.0.4044.138 Safari/537.36'
+        //     }
+        // })
+        // let $ = cheerio.load(resRedirect.body)
 
-        const pipeline = promisify(Stream.pipeline);
+        // const pipeline = promisify(Stream.pipeline);
 
-        await pipeline(
-            got.stream($('video')[0]['attribs']['src']),
-            fs.createWriteStream(`${ctx.from.id}.mp4`)
-        )
+        // await pipeline(
+        //     got.stream($('video')[0]['attribs']['src']),
+        //     fs.createWriteStream(`${ctx.from.id}.mp4`)
+        // )
 
-        ctx.replyWithVideo({source: `${ctx.from.id}.mp4`})
+        // ctx.replyWithVideo({source: `${ctx.from.id}.mp4`})
 
-        fs.unlinkSync(`${ctx.from.id}.mp4`)
+        // fs.unlinkSync(`${ctx.from.id}.mp4`)
 
     } else {
         ctx.reply(`${ctx.message.cmdArgs[1]} is not a valid URL.`)
